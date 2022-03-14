06911f1ac37ecb4e3f70a1e28400ab3449fd5e8a
Elizabeth Adejumo <a.adejumo13@gmail.com>
2022-03-14 22:33:05 +0000

    add custom list

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const _ = require("lodash");
 
 const app = express();
 
@@ -45,6 +46,13 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3]
 
+// creating a list schema for customListName
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("list", listSchema);
 
 
 // index page
@@ -71,36 +79,79 @@ app.get("/", function(req, res) {
   });
 });
 
+// use express paramaters to create a custom list
+app.get("/:customListName", function(req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+  // finding a list in the mongoose database
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if (!foundList){
+        // create a new list
+        const list = new List ({
+          name: customListName,
+          items: defaultItems
+        });
+
+        list.save();
+
+        res.redirect("/" + customListName);
+
+      } else {
+        // show an existing list
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        });
+      }
+    }
+  });
+
+
+
+
+});
+
 // posting to add items
 app.post("/", function(req, res) {
   const newItem = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item({
     name: newItem
   });
-  item.save();
-  res.redirect("/");
+
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 });
 
 // posting to delete items
 app.post("/delete", function(req, res) {
   const checkedItemID = req.body.checkbox;
+  const listName = req.body.listName;
   // delete item from database based on the ID
-  Item.findByIdAndRemove(checkedItemID, function(err) {
-    if (!err) {
-      console.log("Success");
-    }
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemID, function(err) {
+      if (!err) {
+        console.log("Success");
+        res.redirect("/");
+      }
+    });
+  } else {
+    // more efficient way of removing an array item in mongoose
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemID}}}, function(err, foundList){
+      if (!err){
+        res.redirect("/" + listName);
+      }
   });
-  res.redirect("/");
-});
-
-
-// use express routing to create custom dynamic lists
-app.get("/work", function(req, res) {
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems
-  });
+}
 });
 
 app.get("/about", function(req, res) {
